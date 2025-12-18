e3da94bcd9d56f8f44b296b7d12eedd3ce8ca1e9
Pat Buffolino <pat.buffolino@paramount.com>
2025-12-18 15:37:24 -0500

    Update basePath for hackathon-passkey-site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/hackathon-passkey-site',
+  assetPrefix: '/hackathon-passkey-site/',
   reactCompiler: true,
 };
 
